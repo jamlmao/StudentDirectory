072f5abf0e74bfa2cb5ada24d21fc3a319d01700
freyannex <preciousanne.c.abana@isu.edu.ph>
2024-04-16 17:14:00 +0800

    stylelogo

--- index.php
+++ index.php
@@ -39,25 +39,71 @@ if (isset($_POST["btnLogin"])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Style/indexstyle.css">
-    <title>Student Document</title>
+    <title>Login</title>
 </head>
 <body>
-<form action="index.php" method="POST">
-        <h2>TESTING LANG TO</h2>
-        <?php if (!empty($errorMsg)) { ?>
-            <p class="error-message"><?php echo $errorMsg; ?></p>
+    
+    <div class="container">
+        <div class="box form-box">
+        <img src="Style/Images/logo.png">
+        </div>
+
+      <div class="container">
+        <div class="box form-box">
+            
+            <?php 
+             
+              if(isset($_POST['submit'])){
+                $email = mysqli_real_escape_string($con,$_POST['email']);
+                $password = mysqli_real_escape_string($con,$_POST['password']);
+
+                $result = mysqli_query($con,"SELECT * FROM users WHERE Email='$email' AND Password='$password' ") or die("Select Error");
+                $row = mysqli_fetch_assoc($result);
+
+                if(is_array($row) && !empty($row)){
+                    $_SESSION['valid'] = $row['Email'];
+                    $_SESSION['username'] = $row['Username'];
+                    $_SESSION['age'] = $row['Age'];
+                    $_SESSION['id'] = $row['Id'];
+                }else{
+                    echo "<div class='message'>
+                      <p>Wrong Username or Password</p>
+                       </div> <br>";
+                   echo "<a href='index.php'><button class='btn'>Go Back</button>";
+         
+                }
+                if(isset($_SESSION['valid'])){
+                    header("Location: home.php");
+                }
+              }else{
+
+            
+            ?>
+            <header>Login</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
+
+                <div class="field">
+                    
+                    <input type="submit" class="btn" name="submit" value="Login" required>
+                </div>
+                <div class="links">
+                    Don't have account? <a href="register.php">Register here</a>
+                </div>
+            </form>
+        </div>
         <?php } ?>
-
-        <label>Username:</label>
-        <input type="text" name="username"><br>
-
-        <label>Password:</label>
-        <input type="password" name="passcode"><br>
-
-        <button type="submit" name="btnLogin">Login</button>
-        <button id="register-btn" onclick="location.href='register.php'" type="button">Register</button>
-    </form>
+      </div>
 </body>
 </html>
