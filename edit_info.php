d15bbed11e0e7d1648331253fe8f4fc085fdf5ae
Mj Velasco <velascomarkjoshua24@gmail.com>
2024-04-19 17:16:38 +0800

    modified

--- edit_info.php
+++ edit_info.php
@@ -13,36 +13,36 @@
     <div class="col-10">
         <h1>Edit Student Information</h1>
         <?php
-        require("dbconnect.php");
-        session_start();
+require("dbconnect.php");
+session_start();
 
-        if(isset($_POST["btnSave"])){
-            $studID = $_POST["Student_ID"];
-            $frstnm = $_POST["firstname"];
-            $lstnm = $_POST["lastname"];
-            $birthd = $_POST["birthdate"];
-            $haddress = $_POST["homeadd"];
-            $baddress = $_POST["boardingadd"];
-            $connum = $_POST["contact"];
-            $email = $_POST["email"];
-            $sx = $_POST["sex"];
-            $crs = $_POST["Course"];
+if(isset($_POST["btnSave"])){
+    $studID = $_POST["Student_ID"];
+    $frstnm = $_POST["firstname"];
+    $lstnm = $_POST["lastname"];
+    $birthd = $_POST["birthdate"];
+    $haddress = $_POST["homeadd"];
+    $baddress = $_POST["boardingadd"];
+    $connum = $_POST["contact"];
+    $email = $_POST["email"];
+    $sx = $_POST["sex"];
+    $crs = $_POST["Course"];
 
-            if(empty($frstnm) || empty($lstnm) || empty($birthd) || empty($haddress) || empty($baddress) || empty($connum) || empty($sx) || empty($crs)){
-                echo "Please input all required fields.";
-            } else {
-                $sql = "UPDATE `tblstudentinfo` SET `Fname` = :ifrstnm, `Lname` = :ilstnm, `bdate` = :ibirthd, `homeaddr` = :ihaddress, `boardingaddr` = :ibaddress, `contact` = :iconnum, `sex` = :isx, `course` = :icrs WHERE `StudentID` = :istudID";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(':istudID', $studID);
-                $stmt->bindParam(':ifrstnm', $frstnm);
-                $stmt->bindParam(':ilstnm', $lstnm);
-                $stmt->bindParam(':ibirthd', $birthd);
-                $stmt->bindParam(':ihaddress', $haddress);
-                $stmt->bindParam(':ibaddress', $baddress);
-                $stmt->bindParam(':iconnum', $connum);
-                $stmt->bindParam(':isx', $sx);
-                $stmt->bindParam(':icrs', $crs);
-                $stmt->execute();
+    if(empty($frstnm) || empty($lstnm) || empty($birthd) || empty($haddress) || empty($baddress) || empty($connum) || empty($sx) || empty($crs)){
+        echo "Please input all required fields.";
+    } else {
+        $sql = "UPDATE `tblstudentinfo` SET `Fname` = :ifrstnm, `Lname` = :ilstnm, `bdate` = :ibirthd, `homeaddr` = :ihaddress, `boardingaddr` = :ibaddress, `contact` = :iconnum, `sex` = :isx, `course` = :icrs WHERE `StudentID` = :istudID";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':istudID', $studID);
+        $stmt->bindParam(':ifrstnm', $frstnm);
+        $stmt->bindParam(':ilstnm', $lstnm);
+        $stmt->bindParam(':ibirthd', $birthd);
+        $stmt->bindParam(':ihaddress', $haddress);
+        $stmt->bindParam(':ibaddress', $baddress);
+        $stmt->bindParam(':iconnum', $connum);
+        $stmt->bindParam(':isx', $sx);
+        $stmt->bindParam(':icrs', $crs);
+        $stmt->execute();
 
                 if($stmt->rowCount() > 0){
                     echo "Record has been updated!";
