152d564df2d7d7e4c41aabdc6b51f6437a9a5952
Mj Velasco <velascomarkjoshua24@gmail.com>
2024-04-19 17:11:15 +0800

    modified

--- edit_info.php
+++ edit_info.php
@@ -59,7 +59,7 @@
             $studID = $_GET["student_id"];
 
             // Retrieve student data from the database
-            $sql = "SELECT * FROM studentinfo WHERE StudentID = :student_id";
+            $sql = "SELECT * FROM tblstudentinfo WHERE StudentID = :student_id";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':student_id', $studID);
             $stmt->execute();
@@ -75,7 +75,7 @@
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="row">
                 <div class="mb-3 w-50">
                 <label class="form-label mb-0">Student ID: </label>
-                        <input type="text" class="form-control" name="Student_ID" readonly>
+                        <input type="text" class="form-control" name="Student_ID">
                     </div>
 
                     <div class="mb-3 w-50">
